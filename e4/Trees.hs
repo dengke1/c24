2152cfee605ceb53947a3114e7f484322357413e
dengke1 <k.deng123@gmail.com>
2019-01-31 22:33:35 -0500

    Update Trees.hs
    actual assignment?

--- e4/Trees.hs
+++ e4/Trees.hs
@@ -3,21 +3,29 @@ module Trees where
 -- A funny tree.
 data Tree a = Leaf a | Node  (a, Tree a, Tree a)
 
+
+--five = Node (1, 
+--               Node (2, Leaf 3, Leaf 4),
+--               Leaf 5)
+
 -- countNodes t
 -- return the number of nodes in tree t
-countNodes  = 42
+countNodes (Leaf a) = 1
+countNodes (Node (a, b, c)) = 1 + countNodes b + countNodes c
 
 -- forallNodes p t
 -- return whether p is true of every node in tree t
-forallNodes = 42
+forallNodes p (Leaf a) = p a
+forallNodes p (Node (a, b, c)) = p a && forallNodes p b && forallNodes p c
 
 -- existsNode p t
 -- return whether p is true of some node in tree t
-existsNode = 42
-
+existsNode p (Leaf a) = p a
+existsNode p (Node (a, b, c)) = p a || existsNode p b || existsNode p c
 -- inorder t
 -- return a list of nodes in t in inorder traversal order
-inorder = 42
+inorder (Leaf a) = [a]
+inorder (Node (a, b, c)) = inorder b ++ [a] ++ inorder c
 
 tfold f g (Leaf x) = f x
 tfold f g (Node (x, left, right)) = 
@@ -25,10 +33,10 @@ tfold f g (Node (x, left, right)) =
 
 tfold:: (a -> b) -> (a -> b -> b -> b) -> Tree a -> b
 
-countNodes' = 42
+countNodes' = tfold (const 1) (\ _ x y -> x + y + 1)
 
-forallNodes' = 42
+forallNodes' p = tfold p (\ x l r -> p x && l && r)
 
-existsNode' = 42
+existsNode' p = tfold p (\ x l r -> p x || l || r)
 
-inorder' = 42
+inorder' = tfold (: []) (\ x l r -> l ++ [x] ++ r)
